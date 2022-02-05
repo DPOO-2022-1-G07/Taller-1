3c60ae9ba8a72dd4d0adaafe8b0b8612f4978e77
Juan Andrés Eslava Tovar <88861582+juaneslava@users.noreply.github.com>
2022-02-05 18:50:01 -0500

    Clase Pedido terminada

---
+++ src/uniandes/dpoo/taller1/modelo/Pedido.java
@@ -0,0 +1,128 @@
+package uniandes.dpoo.taller1.modelo;
+
+import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class Pedido {
+	
+	private int numeroPedidos;
+	private int idPedido;
+	private String nombreCliente;
+	private String direccionCliente;
+	private ArrayList<Producto> itemsPedido;
+	
+	
+	public Pedido(int numeroPedidos, int idPedido, String nombreCliente, String direccionCliente, Producto itemsPedido)
+	{
+		this.numeroPedidos = numeroPedidos;
+		this.idPedido = idPedido;
+		this.nombreCliente = nombreCliente;
+		this.direccionCliente = direccionCliente;
+		this.itemsPedido = new ArrayList<>();
+	}
+	
+	public int getNumeroPedidos()
+	{
+		return numeroPedidos;
+	}
+
+	public int getIdPedido()
+	{
+		return idPedido;
+	}
+	
+	public String getNombreCliente()
+	{
+		return nombreCliente;
+	}
+	
+	public String getDireccionCliente()
+	{
+		return direccionCliente;
+	}
+	
+
+	
+	public void agregarProducto(Producto nuevoItem)
+	{
+		itemsPedido.add(nuevoItem);
+	}
+	
+	private int getPrecioNetoPedido()
+	{
+		int precioNeto=0;
+		ArrayList<Producto> items = itemsPedido;
+		for (Producto item : items)
+		{
+		precioNeto += item.getPrecio();
+		}		
+		return precioNeto;
+	}
+	
+	private int getPrecioIvaPedido()
+	{
+		int totalIva=0;
+		int totalNeto=getPrecioNetoPedido();
+		totalIva= (int) ( totalNeto * 0.19);
+		return totalIva;
+	}
+	
+	private int getPrecioTotalPedido()
+	{
+		int totalIva = getPrecioIvaPedido();
+		int totalNeto = getPrecioNetoPedido();
+		int totalPedido = 0;
+		totalPedido = totalIva + totalNeto;
+		return totalPedido;
+	}
+	
+	public void guardarFactura(File archivo)
+	{
+		try {
+            String ruta = "./docs/Factura pedido.txt";
+            String contenido = "";
+            ArrayList<Producto> items = itemsPedido;
+    		for (Producto item : items)
+            {contenido = item.getNombre() + "........" + item.getPrecio()+"\n";
+            }
+            contenido = contenido + "Precio neto" + "........" + getPrecioNetoPedido() + "\n";
+            contenido = contenido + "Precio IVA" + "........" + getPrecioIvaPedido() + "\n";
+            contenido = contenido + "Precio TOTAL" + "........" + getPrecioTotalPedido();
+            File file = new File(ruta);
+            // Si el archivo no existe es creado
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(contenido);
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
